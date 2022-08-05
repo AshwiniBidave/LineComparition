6ee84c225a49cac9e3ac36894f5e33aba6f749e2
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-08-05 16:05:34 +0530

    implemented uc2 code

--- uc_1/uc_1/Program.cs
+++ uc_1/uc_1/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace uc_1
+namespace uc_2
 {
     class Program
     {
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Enter End Points of First line");
+            Console.WriteLine("Enter x1 value:");
             int x1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter x2 value:");
             int x2 = Convert.ToInt32(Console.ReadLine());
@@ -14,9 +16,22 @@ namespace uc_1
             int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter y2 value:");
             int y2 = Convert.ToInt32(Console.ReadLine());
-            double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            double lengthOfFirstLine = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("..............................................................");
+            Console.WriteLine("Enter End Points of Second line");
+            Console.WriteLine("Enter a1 value:");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a2 value:");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b1 value:");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b2 value:");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            double lengthOfSecondLine = Math.Sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+            Console.WriteLine("Legth of line is: " + lengthOfFirstLine);
+            Console.WriteLine("Legth of line is: " + lengthOfSecondLine);
+            Console.WriteLine(lengthOfFirstLine.Equals(lengthOfSecondLine));
 
-            Console.WriteLine("Legth of line is: " + length);
         }
     }
 }
